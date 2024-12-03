d8f13a477cc6fc2cb68c854a2771323186ce55da
mairabiachi <mairaamericano2@gmail.com>
2024-12-03 10:00:06 -0300

    Add files via upload
    aula dois sobre python,variaveis

---
+++ aula 2/aula2.py
@@ -0,0 +1,7 @@
+print("hello,my name is lucy")
+name_user = "bylucy" #tipo-> str = string
+old_user = 18 # tipo -> int = integer
+height_user = 1.57 # tipo -. float = ponto flutuante
+off_legal_age = True # tipo -> bool = boolean
+print ("welcome",name_user)
+print (("hello,my name is lucy"), name_user, ("your age is?"), old_user, ("your heigth is"), height_user, ("you or off age?"), off_legal_age)
